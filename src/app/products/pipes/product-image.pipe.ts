4543fbfb694c71897147130485550604ab8c405c
Adrian-ortiz0 <dxniel7328@gmail.com>
2026-02-12 10:36:36 -0500

    fin del panel

--- src/app/products/pipes/product-image.pipe.ts
+++ src/app/products/pipes/product-image.pipe.ts
@@ -3,22 +3,22 @@ import { environment } from 'src/environments/environment';
 
 const baseUrl = environment.BASE_URL;
 
-
 @Pipe({
-    name: 'productImage'
+  name: 'productImage',
 })
-
-
 export class ProductImagePipe implements PipeTransform {
-    transform(value: string | string[]): string {
-        if(typeof value === 'string'){
-            return `${baseUrl}/files/product/${value}`
-        }
-        const image = value.at(0);
-
-        if(!image) {
-            return './assets/images/no-image.jpg'
-        }
-        return `${baseUrl}/files/product/${image}`
+  transform(value: null | string | string[]): string {
+    if (value === null) {
+      return './assets/images/no-image.jpg';
     }
+    if (typeof value === 'string') {
+      return `${baseUrl}/files/product/${value}`;
+    }
+    const image = value.at(0);
+
+    if (!image) {
+      return './assets/images/no-image.jpg';
+    }
+    return `${baseUrl}/files/product/${image}`;
+  }
 }
--- src/app/products/services/products.service.ts
+++ src/app/products/services/products.service.ts
@@ -1,8 +1,13 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
-import { Product, ProductsResponse } from '../interfaces/product.interface';
+import {
+  Gender,
+  Product,
+  ProductsResponse,
+} from '../interfaces/product.interface';
 import { Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { User } from '@/auth/interfaces/auth.interface';
 
 const baseUrl = environment.BASE_URL;
 
@@ -11,6 +16,20 @@ interface Options {
   offset?: number;
   gender?: string;
 }
+
+const emptyProduct: Product = {
+  id: 'new',
+  title: '',
+  price: 0,
+  description: '',
+  slug: '',
+  stock: 0,
+  sizes: [],
+  gender: Gender.Men,
+  tags: [],
+  images: [],
+  user: {} as User,
+};
 
 @Injectable({ providedIn: 'root' })
 export class ProductsService {
@@ -51,7 +70,11 @@ export class ProductsService {
       .pipe(tap((product) => this.productCache.set(idSlug, product)));
   }
 
-  getProductById(id: string):Observable<Product>{
+  getProductById(id: string): Observable<Product> {
+    if (id === 'new') {
+      return of(emptyProduct);
+    }
+
     if (this.productCache.has(id)) {
       return of(this.productCache.get(id)!);
     }
@@ -59,5 +82,33 @@ export class ProductsService {
     return this.http
       .get<Product>(`${baseUrl}/products/${id}`)
       .pipe(tap((product) => this.productCache.set(id, product)));
+  }
+
+  updateProduct(
+    id: string,
+    productLike: Partial<Product>,
+  ): Observable<Product> {
+    return this.http
+      .patch<Product>(`${baseUrl}/products/${id}`, productLike)
+      .pipe(tap((product) => this.updateProductCache(product)));
+  }
+
+  createProduct(productLike: Partial<Product>): Observable<Product> {
+    return this.http
+      .post<Product>(`${baseUrl}/products`, productLike)
+      .pipe(tap((product) => this.updateProductCache(product)));
+  }
+  updateProductCache(product: Product) {
+    const productId = product.id;
+
+    this.productCache.set(productId, product);
+
+    this.productsCache.forEach((productResponse) => {
+      productResponse.products = productResponse.products.map(
+        (currentProduct) => {
+          return currentProduct.id === productId ? product : currentProduct;
+        },
+      );
+    });
   }
 }
